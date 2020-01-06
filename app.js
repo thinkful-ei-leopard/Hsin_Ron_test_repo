6f8a2c54c1bf1bcff85517015e25ab6554f730dd
Ron Martin <ronmartin89@gmail.com>
2020-01-06 11:54:10 -0800

    next step

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ let myTeam = 'Hsin and Ron';
 
 console.log(myTeam);
 
-console.log('Hi my name is Ron and Im 30 years old.');
-
 function createGreeting (){
-    console.log('Hi my name is Ron and Im 30 years old.');
+    return 'Hi my name is Ron and Im 30 years old.';
 }
-
-createGreeting ();
+const greeting1 = createGreeting();
+console.log(greeting1);
