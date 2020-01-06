a8234759d602a39050e2f3f5c4101ea2e2210cdb
Hsin Ling Hu <hsinkava11@gmail.com>
2020-01-06 11:52:22 -0800

    create a function

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ let myTeam = 'Hsin and Ron';
 console.log(myTeam);
 
 console.log('Hi my name is Ron and Im 30 years old.');
+
+function createGreeting (){
+    console.log('Hi my name is Ron and Im 30 years old.');
+}
+
+createGreeting ();
